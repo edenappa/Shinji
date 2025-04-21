369ae3477b05a56ef72715877f0c0e23300a88fb
edenappa <remon1143@gmail.com>
2025-04-22 01:24:38 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>SilentService / Shinji</h1>
+      <p>이건 404 오류를 제거하기 위한 테스트 페이지입니다.</p>
+    </div>
+  );
+}
